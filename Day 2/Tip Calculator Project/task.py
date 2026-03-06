09ac1c7a87415acee7ab2e9996938a4d10a4c5bd
harshg <harshgarg698@gmail.com>
2026-03-06 15:26:43 +0530

    Day 2 complete

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -2,5 +2,6 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
-
-
+total_tip=(bill*tip/100)
+per_head=round(float((bill+total_tip)/people),2)
+print(f"Each person should pay: ${per_head}")
