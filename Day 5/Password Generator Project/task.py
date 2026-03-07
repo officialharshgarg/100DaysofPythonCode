f2124ad39a0fd740709b38e966b55942feed9799
harshg <harshgarg698@gmail.com>
2026-03-07 19:18:27 +0530

    Day 5 complete

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,4 +1,8 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -7,4 +11,28 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+i = 0
+j = 0
+k = 0
+password = ""
+for l in letters:
+    if i < nr_letters:
+        password += random.choice(letters)
+    i += 1
+for s in symbols:
+    if k < nr_symbols:
+        password += random.choice(symbols)
+        k += 1
+for n in numbers:
+    if j < nr_numbers:
+        password += random.choice(numbers)
+    j += 1
+print(password)
 
+#Random positions
+password = list(password)
+random.shuffle(password)
+pas=""
+for p in password:
+    pas += p
+print(pas)
