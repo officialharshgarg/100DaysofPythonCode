3bafbaecf1472b229dc3f41abb4971811f9510a7
harshg <harshgarg698@gmail.com>
2026-03-08 23:35:19 +0530

    updated

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -1,9 +1,9 @@
 #Creating the function
-def get_username():
+def name():
     name = input("What is your name? ")
     print("Hello, " + name)
     # Inside the function
 
 #Outside the function
 print("Hello")
-get_username() # Calling the function
+name() # Calling the function
