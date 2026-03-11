f450831a7911c49d5edcf961cbadb0c3bf07d738
harshg <harshgarg698@gmail.com>
2026-03-11 23:33:16 +0530

    updated

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -9,3 +9,4 @@ word_list = ["aardvark", "baboon", "camel"]
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
 chosen_word = random.choice(word_list)
+guess=input("Guess a letter: ").lower()
