9fb8b47d563816c475d88fb2b59ef4ee37c1d525
harshg <harshgarg698@gmail.com>
2026-03-14 00:32:02 +0530

    hangman game building

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -9,18 +9,22 @@ word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 print(placeholder)
-correct
-def check_letter(placeholder,word_length):
- while word_length > 0:
+correct_letters = []
+for letter in chosen_word:
+ def check_letter(placeholder,word_length):
+  while word_length > 0:
     guess = input("Guess a letter: ").lower()
     for letter in chosen_word:
        if letter == guess:
            placeholder += letter
            word_length -= 1
+       elif letter == guess:
+               correct_letters.append(letter)
        else:
            placeholder += "_"
     print(placeholder)
 placeholder = ""
 check_letter(placeholder, word_length)
+print(correct_letters)
 
 
