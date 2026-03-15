b04e1816e7fdace696a433dfc23937ce2c5e9b86
harshg <harshgarg698@gmail.com>
2026-03-15 23:42:40 +0530

    hangman game building

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,6 +1,63 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 
+lives=6
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -15,7 +72,6 @@ correct_letters = []
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
-
     display = ""
 
     for letter in chosen_word:
@@ -29,6 +85,15 @@ while not game_over:
 
     print(display)
 
+    if guess not in chosen_word:
+        lives -= 1
+        if lives<0:
+            game_over = True
+            print("You Lose!")
+    elif guess in correct_letters:
+        print(stages[lives])
+
     if "_" not in display:
         game_over = True
         print("You win.")
+
