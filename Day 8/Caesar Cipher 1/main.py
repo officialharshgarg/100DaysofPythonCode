05858c6e4b360e3f0b289980e4b8117279b2270e
harshg <harshgarg698@gmail.com>
2026-03-19 14:09:38 +0530

    Caesar Cipher1 start day8

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -1,15 +1,25 @@
- alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+def encrypt(original_text, shift):
+    for letter in original_text:
+        if letter not in alphabet:
+            print("Invalid Input")
+        elif letter in alphabet and shift < len(alphabet):
+            pos=alphabet.index(letter)
+            if pos==len(alphabet)-1:
+                pos=0
+                encrypt_char = alphabet[pos+shift]
+            elif pos>19 and shift >6:
+                pos
+        elif shift > len(alphabet):
+                excess=shift-len(alphabet)
+                pos=shift
+                encrypt_char = alphabet[pos+shift]
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-def encrypt():
-    original_text = input("Type text:\n")
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
 
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
