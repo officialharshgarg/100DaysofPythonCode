e37f25d2fe32ff91bb3433c067b6fa4f9ced6e6c
harshg <harshgarg698@gmail.com>
2026-03-20 19:35:57 +0530

    Caesar Cipher1 update

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,32 +1,29 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
-
-
+from art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# TODO-2: What happens if the user enters a number/symbol/space?
-
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
 
     for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
-
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if letter in alphabet:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        elif letter not in alphabet:
+            output_text += letter
     print(f"Here is the {encode_or_decode}d result: {output_text}")
+    choice = input("Want to restart cipher? yes or no:\n").lower()
+    return choice
 
-
-# TODO-3: Can you figure out a way to restart the cipher program?
-
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+choice = "yes"
+while choice != "no":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    choice=caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
 
 
