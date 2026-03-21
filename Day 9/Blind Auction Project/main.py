dc5a17f1570d970f55d73360e4c782931af45478
harshg <harshgarg698@gmail.com>
2026-03-21 22:00:27 +0530

    Day 9 & Auction program complete

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,22 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
-
-
+from art import logo
+print(logo)
+print("Welcome to auction of luxury")
+bidding = "on"
+bid={}
+while bidding != "over":
+    bidder=input("What's name of bidder?")
+    amount=int(input("How much would you like to bid?"))
+    bid[bidder]=amount
+    more_bidders=(input("Are there more bidders? (yes/no)")).lower()
+    if more_bidders == "no": bidding="over"
+    else: print("\n"* 30)
+max=0
+winner=''
+for key in bid:
+     if bid[key] > max:
+         max=bid[key]
+         winner=key
+print(f"Auction Won by {winner} with bid amount ${max}")
