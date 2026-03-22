7dcc43656ae7dda12c21a6faf1705e11e08c8dfa
harshg <harshgarg698@gmail.com>
2026-03-23 01:22:03 +0530

    Day 11 BlackJack Game

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,3 +1,16 @@
+import random
 from art import logo
 print(logo)
+print("Game begins now, may the lucky one wins!")
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+game="on"
+user=[]
+computer=[]
+u_score=0
+c_score=0
+while game != "off":
+    user=random.choices(cards,k=2)
+    computer=random.choices(cards,k=2)
+
+
+
