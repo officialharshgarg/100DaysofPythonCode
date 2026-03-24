cfd8a398d6caee7e1670efba88f8d3cd2c214e4c
harshg <harshgarg698@gmail.com>
2026-03-24 18:41:17 +0530

    Day 12 & Number guessing game completed

---
+++ Day 12/Number Guessing Project/main.py
@@ -0,0 +1,30 @@
+import random
+from art import logo
+print(logo)
+print("Welcome to number guessing game\nI'm thinking of number between 1 to 100.")
+
+level=input("would you like easy or hard difficulty. Type 'easy' or 'hard'?")
+num=random.randint(1,100)
+guess=0
+
+def guess_num(num, g):
+    global guess
+    limit = g+1
+    print(f"You have got {g} guesses to find the correct number.")
+    for n in range(1, limit):
+        if n != g+1:
+            while num != guess:
+                guess = input("Make a guess:")
+                if guess < num:
+                    print("Your guess is too low.")
+                elif guess > num:
+                    print("Your guess is too high.")
+                elif guess == num:
+                    print("You guessed the correct number.")
+        elif n == limit and guess != num:
+            print("You ran out of guesses.")
+
+if level == "easy":
+    guess_num(num, 10)
+elif level == "hard":
+    guess_num(num, 5)
